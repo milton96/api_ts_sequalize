d96e9ee7086f97dab7c381686918f6b2812109f5
Milton Bravo <miltonbravo96@gmail.com>
2023-08-08 23:32:13 -0600

    Se crea servidor basico

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!!");
+import app from "./app";
+
+const main = async () => {
+    app.listen(3000, () => {
+        console.log(`Servidor en el puerto 3000`);
+    });
+};
+
+main();
