e988be86546a4022c6570eb49bcc6575a35ab5d8
Milton Bravo <miltonbravo96@gmail.com>
2023-08-09 02:32:26 -0600

    Se estandariza la respuesta en el login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,20 +1,22 @@
 import { Request, Response } from "express";
 
-import statusCodes from "../status_codes";
+import codesHttp from "../status_codes";
+import ResponseHelper from "../helpers/response";
 
 const test = async (req: Request, res: Response) => {
   return res.send("<h1>Hola mundo</h1>");
 };
 
 const login = async (req: Request, res: Response) => {
+  const response = new ResponseHelper();
   try {
     const info = req["body"];
-    console.log(info);
 
-    return res.status(statusCodes["OK"]["code"]).end();
+    response.setResponse(info);
+    return response.getResponse(res, codesHttp["OK"]);
   } catch (error: any) {
-    if (error["desc"]) return res.status(error.desc).json({ error });
-    return res.status(500).json({ error });
+    response.setError(error);
+    return response.getResponse(res, null);
   }
 };
 
