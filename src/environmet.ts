8ae140ac55eb02b562c106af1f081e7a584f7b42
Milton Bravo <miltonbravo96@gmail.com>
2023-08-14 22:08:11 -0600

    Se agregan las variables de entorno para conectar a la base de datos

--- src/environmet.ts
+++ src/environmet.ts
@@ -9,6 +9,13 @@ const env = {
   server: {
     PORT: parseInt(process.env["PORT"] || "3000", 10),
   },
+  db: {
+    DB_DIALECT: process.env["DB_DIALECT"] || "postgres",
+    DB_NAME: process.env["DB_NAME"] || "test",
+    DB_HOST: process.env["DB_HOST"] || "test",
+    DB_USER: process.env["DB_USER"] || "test",
+    DB_PASSWORD: process.env["DB_PASSWORD"] || "test",
+  }
 };
 
 export default env;
